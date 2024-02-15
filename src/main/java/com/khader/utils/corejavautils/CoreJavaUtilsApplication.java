128c3db67040ac6f90c4bf1a28289038b4172e6f
Khader-Ajex <khader.basha@aj-ex.com>
2024-02-15 17:58:17 +0300

    initial commit

---
+++ src/main/java/com/khader/utils/corejavautils/CoreJavaUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.khader.utils.corejavautils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khader/utils/corejavautils/CoreJavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khader.utils.corejavautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreJavaUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
